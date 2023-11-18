c5b098cd8883e46ba83b53ec3a9f72a2485df557
JYing <jyab0617@gmail.com>
2023-11-18 18:01:22 -0500

    create bullish engulfing

---
+++ COP4365 P2/PatternRecognizers/MultiCsPatternRecognizers/BullishEngulfing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COP4365_P3.PatternRecognizers.MultiCsPatternRecognizers
+{
+    internal class BullishEngulfing : Recognizer
+    {
+        public BullishEngulfing() : base("BullishEngulfing", 2) { }
+
+        public override bool RecognizePattern(List<smartCandlestick> lscs)
+        {
+            smartCandlestick smallerScs = lscs[0];
+            smartCandlestick bullishScs = lscs[1];
+            return bullishScs.isBullish;
+            
+        }
+    }
+}
